ba3bac7cd5acaf3c06eb5bcd382006307c2c52e7
Francisco Baena <yo@baenafrancisco.com>
2014-01-20 19:51:23 +0100

    Añadido modelo

--- www/models.py
+++ www/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Persona(models.Model):
 	nombre = models.TextField(max_length=80)
-	apellidos = models.TextField(max_length=100)
+	apellidos = models.TextField(max_length=100, null=True, blank=True)
 	telefono = models.TextField(max_length=20)
+	email = models.TextField(max_length=70)
 	def __unicode__(self):
 		return "%s %s" % (self.nombre, self.apellidos)
