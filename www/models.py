6d73f21f7fb4ddc846cd00a3a7cf96dbb128565a
Francisco Baena <yo@baenafrancisco.com>
2014-01-20 19:13:20 +0100

    Añadido modelo "Persona"

--- www/models.py
+++ www/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Persona(models.Model):
+	nombre = models.TextField(max_length=80)
+	apellidos = models.TextField(max_length=100)
+	telefono = models.TextField(max_length=20)
+	def __unicode__(self):
+		return "%s %s" % (self.nombre, self.apellidos)
